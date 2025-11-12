fd7a6889acd0952669407dd4b4f8b2ac16b67b0c
sergeipavlov <s.n.pavlov@outlook.com>
2025-11-12 23:09:43 +0300

    Add sticky-banner YANDEX

--- app/src/main/java/site/metrolog/metrolog/MainActivity.java
+++ app/src/main/java/site/metrolog/metrolog/MainActivity.java
@@ -1,24 +1,99 @@
 package site.metrolog.metrolog;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.yandex.mobile.ads.banner.BannerAdEventListener;
+import com.yandex.mobile.ads.banner.BannerAdSize;
+import com.yandex.mobile.ads.banner.BannerAdView;
+import com.yandex.mobile.ads.common.AdRequest;
+import com.yandex.mobile.ads.common.AdRequestError;
+import com.yandex.mobile.ads.common.ImpressionData;
+import com.yandex.mobile.ads.common.MobileAds;
+
 public class MainActivity extends AppCompatActivity {
+    @Nullable
+    private BannerAdView bannerAdView;
+    private View adContainerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        adContainerView = findViewById(R.id.main);
+        bannerAdView = findViewById(R.id.ad_container_view);
+
+        adContainerView.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override
+                    public void onGlobalLayout() {
+                        adContainerView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                        BannerAdSize size = getStickyAdSize();
+                        loadStickyBanner(size);
+                    }
+                }
+        );
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @NonNull
+    private BannerAdSize getStickyAdSize() {
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        int adWidthPx = adContainerView.getWidth();
+        if (adWidthPx == 0) {
+            adWidthPx = dm.widthPixels;
+        }
+        int adWidthDp = Math.round(adWidthPx / dm.density);
+        return BannerAdSize.stickySize(this, adWidthDp);
+    }
+
+    private void loadStickyBanner(@NonNull BannerAdSize adSize) {
+        if (bannerAdView == null) return;
+
+        bannerAdView.setAdSize(adSize);
+
+        bannerAdView.setAdUnitId("demo-banner-yandex"); // <-- тестовый ID
+
+        bannerAdView.setBannerAdEventListener(new BannerAdEventListener() {
+            @Override public void onAdLoaded() {
+                if (isDestroyed() && bannerAdView != null) bannerAdView.destroy();
+            }
+            @Override public void onAdFailedToLoad(@NonNull AdRequestError error) {
+
+            }
+            @Override public void onAdClicked() { }
+            @Override public void onLeftApplication() { }
+            @Override public void onReturnedToApplication() { }
+            @Override public void onImpression(@Nullable ImpressionData data) { }
+        });
+
+        AdRequest request = new AdRequest.Builder().build();
+        bannerAdView.loadAd(request);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (bannerAdView != null) {
+            bannerAdView.destroy();
+            bannerAdView = null;
+        }
+        super.onDestroy();
     }
 }
