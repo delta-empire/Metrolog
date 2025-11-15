96a28d797d089ffd4f0dff76fbe4b62683b49e16
Delta Empire <65801117+delta-empire@users.noreply.github.com>
2025-11-15 19:53:25 +0300

    Add mass conversion screen with sticky banner

--- app/src/main/java/site/metrolog/metrolog/UnitsActivity.java
+++ app/src/main/java/site/metrolog/metrolog/UnitsActivity.java
@@ -57,6 +57,7 @@ public class UnitsActivity extends AppCompatActivity {
 
         List<Integer> items = Arrays.asList(
                 R.string.units_option_temperature,
+                R.string.units_option_mass,
                 R.string.units_option_speed,
                 R.string.units_option_acceleration,
                 R.string.units_option_time,
@@ -85,6 +86,8 @@ public class UnitsActivity extends AppCompatActivity {
     private void handleUnitClick(@StringRes int optionResId) {
         if (optionResId == R.string.units_option_temperature) {
             startActivity(new Intent(this, TemperatureConversionActivity.class));
+        } else if (optionResId == R.string.units_option_mass) {
+            startActivity(new Intent(this, MassConversionActivity.class));
         } else if (optionResId == R.string.units_option_speed) {
             startActivity(new Intent(this, SpeedConversionActivity.class));
         } else if (optionResId == R.string.units_option_acceleration) {
